7748dcda5f19f6f0bf6485a2ef5f8970be442634
Аркадий Алфёров <arhe414@gmail.com>
2025-08-15 16:55:00 +0300

    Добавил модель ошибки

--- Frendy.Shared/Exceptions/UserException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Frendy.Shared.Exceptions;
-
-/// <summary>
-/// Исключение пользователя
-/// </summary>
-public class UserException : Exception
-{
-    public UserException(string message) : base(message) {}
-}
